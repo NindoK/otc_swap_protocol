943168202c923dd218db231c3f4955fad5087392
SakshiShah29 <sakshi.shah299@gmail.com>
2023-06-05 11:52:05 +0530

    Responsiveness added

--- frontend/src/app/createdeal/page.js
+++ frontend/src/app/createdeal/page.js
@@ -48,17 +48,18 @@ const CreateDeal = () => {
                     <ConnectButton showBalance={false} />
                 </div>
             </div>
-            <div className="flex h-screen w-full bg-black">
+            <div className="flex lg:h-screen w-full bg-black">
                 {/* gradient start */}
                 <div className="absolute z-[0] w-[40%] h-[35%] top-0 right-0 pink__gradient" />
                 <div className="absolute z-[0] w-[40%] h-[50%] rounded-full right-0 white__gradient bottom-40" />
                 <div className="absolute z-[0] w-[50%] h-[50%] left-0 bottom-40 blue__gradient" />
 
-                <form className="bg-transparent text-white h-fit w-fit p-10">
-                    <h2>Create a Deal</h2>
-                    <div className="flex gap-14">
-                    <FormControl isRequired isInvalid={isError} className="flex flex-col my-5">
-                        <FormLabel>Underlying Token</FormLabel>
+                <div className="flex justify-center  w-full">
+                <form className="  text-gray-400 font-montserrat h-fit w-fit lg:p-10 p-6  bg-white bg-opacity-5 shadow-md  backdrop-blur rounded-lg border border-gray-400 border-opacity-18 ">
+                    <h2 className="text-center font-bold font-montserrat text-2xl">Create a Deal</h2>
+                    <div className="lg:flex gap-14 ">
+                    <FormControl isRequired isInvalid={isError} className="flex flex-col lg:my-5 my-3">
+                        <FormLabel className=" font-bold">Underlying Token</FormLabel>
                         <Input
                             variant="flushed"
                             placeholder="Address"
@@ -75,7 +76,7 @@ const CreateDeal = () => {
                     </FormControl>
 
                     <FormControl isRequired isInvalid={isError} className="flex flex-col my-5">
-                        <FormLabel>Quote Token</FormLabel>
+                        <FormLabel className=" font-bold">Quote Token</FormLabel>
                         <Input
                             variant="flushed"
                             placeholder="Address"
@@ -90,16 +91,16 @@ const CreateDeal = () => {
                     </FormControl>
                     </div>
 
-                    <div className="flex gap-14">
+                    <div className="lg:flex gap-14">
                     <FormControl isRequired isInvalid={isError} className="flex flex-col my-5">
-                        <FormLabel>Strike</FormLabel>
+                        <FormLabel className=" font-bold">Strike</FormLabel>
                         <NumberInput value={strike} onChange={handleStrikeChange} precision={3}>
                             <NumberInputField />
                         </NumberInput>
                     </FormControl>
 
                     <FormControl isRequired isInvalid={isError} className="flex flex-col my-5">
-                        <FormLabel>Maturity</FormLabel>
+                        <FormLabel className=" font-bold">Maturity</FormLabel>
                         <Input
                             placeholder="Select Date and Time"
                             size="md"
@@ -110,9 +111,9 @@ const CreateDeal = () => {
                     </FormControl>
                     </div>
 
-                    <div className="flex gap-14">
+                    <div className="lg:flex gap-14">
                     <FormControl isRequired isInvalid={isError} className="flex flex-col my-5">
-                        <FormLabel>Call or Put</FormLabel>
+                        <FormLabel className=" font-bold">Call or Put</FormLabel>
                         <Select placeholder="-None-" value={option} onChange={handleOptionChange}>
                             <option>Call</option>
                             <option>Put</option>
@@ -120,7 +121,7 @@ const CreateDeal = () => {
                     </FormControl>
 
                     <FormControl isRequired isInvalid={isError} className="flex flex-col my-5">
-                        <FormLabel>Amount</FormLabel>
+                        <FormLabel className=" font-bold">Amount</FormLabel>
                         <NumberInput value={amount} onChange={handleAmountChange} precision={4}>
                             <NumberInputField />
                         </NumberInput>
@@ -128,9 +129,9 @@ const CreateDeal = () => {
                     </div>
 
 
-                    <div className="flex gap-14">
+                    <div className="lg:flex gap-14">
                     <FormControl isRequired isInvalid={isError} className="flex flex-col my-5">
-                        <FormLabel>Side</FormLabel>
+                        <FormLabel className=" font-bold">Side</FormLabel>
                         <Select placeholder="-None-" value={side} onChange={handleSideChange}>
                             <option>Buy</option>
                             <option>sELL</option>
@@ -138,15 +139,20 @@ const CreateDeal = () => {
                     </FormControl>
                     
                     <FormControl isRequired isInvalid={isError} className="flex flex-col my-5">
-                        <FormLabel>Premium</FormLabel>
+                        <FormLabel className=" font-bold">Premium</FormLabel>
                         <NumberInput value={premium} onChange={handlePremiumChange} precision={4}>
                             <NumberInputField />
                         </NumberInput>
                     </FormControl>
                     </div>
 
-                    
+              <div className="w-full flex justify-center ">
+              <Button className="bg-blue-gradient rounded-xl h-fit w-fit py-2  px-4" type="submit">
+              Submit
+            </Button>
+              </div>
                 </form>
+                </div>
             </div>
         </React.Fragment>
     )
