c83e916a31c0d32dbd3dd9ddbdb1b39768a847d3
Aleksander Wojcik <aleksander.w1992@gmail.com>
2023-06-07 01:27:17 +0200

    add description to use cast

--- script/DeployLocal.sol
+++ script/DeployLocal.sol
@@ -12,8 +12,14 @@ import "../src/OtcToken.sol";
 For local deployment use commands:
 anvil
 // pick from there private key and public key
-export pk = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
-forge script ./script/DeployLocal.sol:DeployLocal --fork-url http://localhost:8545 --private-key $pk —broadcast
+export pk=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+forge script ./script/DeployLocal.sol:DeployLocal --fork-url http://localhost:8545 --private-key $pk --broadcast --resume
+
+verify cast is working:
+cast call 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0 "rfsIdCounter()"
+0x0000000000000000000000000000000000000000000000000000000000000001
+
+
 
 */
 contract DeployLocal is Script {
